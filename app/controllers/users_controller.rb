7a62394fe80379d1eebc19ca77e8b47350ff3667
katahabaR3sei <hayashi.yutaro1118@gmail.com>
2020-04-01 01:33:09 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.now[:notice] = "登録が完了しました。"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "登録に失敗しました。"
+      render new_user_path
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pages#top"
+
+  resources :users
+  get     'login',   to: 'sessions#new'
+  post    'login',   to: 'sessions#create'
+  delete  'logout',  to: 'sessions#destroy'
+end
