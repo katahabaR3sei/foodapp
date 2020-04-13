6544bf1a3ea731e1971d82203bade8e2f5035a94
katahabaR3sei <hayashi.yutaro1118@gmail.com>
2020-04-13 12:29:47 +0900

    shopとpostの関連づけを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,23 @@
 class PostsController < ApplicationController
   def new
-    @post = Post.new
+    @shop = Shop.find(params[:shop_id])
+    @post = @shop.posts.new
+    @user = current_user
   end
 
   def create
+    @shop = Shop.find(params[:shop_id])
+    @post = @shop.posts.new(post_params)
+    if @post.save
+      redirect_to shop_path, notice:"投稿ありがとうございます！"
+    else
+      flash[alert] = "投稿失敗・・・"
+      render new_post_path(@post)
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description, :shop_id, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   root "pages#top"
 
   resources :users
-  resources :shops
-  resources :posts
+  resources :shops do
+    resources :posts
+  end
   get     'login',   to: 'sessions#new'
   post    'login',   to: 'sessions#create'
   delete  'logout',  to: 'sessions#destroy'
