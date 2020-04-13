293b4052db8772059118099b2fbc234a5ed8a7c4
katahabaR3sei <hayashi.yutaro1118@gmail.com>
2020-04-13 19:07:49 +0900

    クチコミ投稿に星評価機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :description, :shop_id, :user_id)
+    params.require(:post).permit(:title, :description, :shop_id, :user_id, :rate)
   end
 end
