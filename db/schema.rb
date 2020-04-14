aa06bfde4195e4e85978edb9369d8f95d993413e
katahabaR3sei <hayashi.yutaro1118@gmail.com>
2020-04-14 09:44:33 +0900

    User登録に画像保存機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_223431) do
+ActiveRecord::Schema.define(version: 2020_04_14_004212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_223431) do
     t.integer "age"
     t.string "sex"
     t.integer "address_id"
+    t.string "image"
   end
 
 end
