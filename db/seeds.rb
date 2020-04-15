e569a60c658cdf4c3783757f0cf2840aab024f63
katahabaR3sei <hayashi.yutaro1118@gmail.com>
2020-04-15 21:12:27 +0900

    seedファイル変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ CSV.foreach('db/genres.csv') do |row|
   Genre.create(:name => row[0])
 end
 
-require "csv"
+
 CSV.foreach('db/priceranges.csv') do |row|
   Pricerange.create(:price => row[0])
 end
 
-require "csv"
+
 CSV.foreach("db/prefectures.csv") do |row|
   Address.create(:prefectures => row[0])
 end
