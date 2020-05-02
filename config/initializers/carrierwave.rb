f68637f15d9766fdaacb5428b70a21b0924b4399
katahabaR3sei <hayashi.yutaro1118@gmail.com>
2020-05-02 23:43:15 +0900

    本番用画像をAWSのS3に保存するよう設定

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIAYDYGMJ4S3ZPQHOMY',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
+    }
+
+    config.fog_directory  = 'rails-photo-yutaro'
+    config.cache_storage = :fog
+  end
+end
