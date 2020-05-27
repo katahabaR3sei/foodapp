09b75fb17fd76a0ddb65cc3036c866ea2fee95c6
katahabaR3sei <hayashi.yutaro1118@gmail.com>
2020-05-27 13:02:52 +0900

    テストコード追加

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_shop_post_url(shops(:one))
-    assert_response :success
-  end
+
 
 end
